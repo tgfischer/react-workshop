425bb6cf971ce40ab31e4226eea06161f08d109f
Tom Fischer <tom.fischer@magnetforensics.com>
2020-01-21 11:03:10 -0500

    Finished application

--- client/src/components/Menu/hooks.js
+++ client/src/components/Menu/hooks.js
@@ -1,7 +1,35 @@
+import { useEffect, useState, useMemo } from "react";
+import useAxios from "axios-hooks";
+import orderBy from "lodash/orderBy";
+
+import { columns } from "./constants";
+
 export const useMenu = () => {
-  return {};
+  const [{ data, loading }, handleRequest] = useAxios(
+    `${process.env.REACT_APP_API_URL}/api/menu`
+  );
+  useEffect(() => void handleRequest(), [handleRequest]);
+  return {
+    columns,
+    rows: data || [],
+    isLoading: loading
+  };
 };
 
-export const useMenuTable = () => {
-  return {};
+export const useMenuTable = ({ rows }) => {
+  const [state, setState] = useState({});
+  return {
+    ...state,
+    rows: useMemo(() => orderBy(rows, [state.orderBy], [state.direction]), [
+      rows,
+      state.orderBy,
+      state.direction
+    ]),
+    handleSort: id => () =>
+      setState(state => ({
+        orderBy: id,
+        direction:
+          state.orderBy === id && state.direction === "asc" ? "desc" : "asc"
+      }))
+  };
 };
